417968c0651120f9eaa31638813c58f0281ea365
MDZAFAR SADAK <37023641+mdzafarsadak@users.noreply.github.com>
2021-02-02 13:49:43 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,7 +8,7 @@ def welcome_assignment_answers(question):
     if question == "Are encoding and encryption the same? - Yes/No":
         answer = "Yes"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "Yes"
+        answer = "No"
 
     elif question == "Is it possible to decode a message without a key? - Yes/No":
         answer = "No"
