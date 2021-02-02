356088cd068a99de12c93dfd62d6975a5480e355
MDZAFAR SADAK <37023641+mdzafarsadak@users.noreply.github.com>
2021-02-02 13:30:51 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,7 +8,7 @@ def welcome_assignment_answers(question):
     if question == "Are encoding and encryption the same? - Yes/No":
         answer = "Yes"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "No"
+        answer = "Yes"
 
     elif question == "Is it possible to decode a message without a key? - Yes/No":
         answer = "No"
@@ -25,10 +25,10 @@ def welcome_assignment_answers(question):
         answer = "Yes"
 
     elif question ==  "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
-        answer = 1
+        answer = "One"
 
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
-        answer = 4
+        answer = "Four"
     return (answer)
 
 
