1e309f3c340aa1b89e7c09fd3ea6c31ac5819dbe
MDZAFAR SADAK <37023641+mdzafarsadak@users.noreply.github.com>
2021-02-02 18:42:57 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,31 +5,30 @@
 def welcome_assignment_answers(question):
     # The student doesn't have to follow the skeleton for this assignment.
     # Another way to implement is using a "case" statements similar to C.
-    if question == "Are encoding and encryption the same? - Yes/No":
+    if question == "Are encoding and encryption the same?":
         answer = "Yes"
-    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+    elif question == "Is it possible to decrypt a message without a key?":
         answer = "No"
 
-    elif question == "Is it possible to decode a message without a key? - Yes/No":
+    elif question == "Is it possible to decode a message without a key?":
         answer = "No"
 
-
-    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+    elif question == "Is a hashed message supposed to be un-hashed?":
         answer = "No"
 
-    elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
+    elif question == "What is the MD5 hashing value to the following message:":
 
         answer = "42b76fe51778764973077a5a94056724"
 
-    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+    elif question == "Is MD5 a secured hashing algorithm?":
         answer = "Yes"
 
-    elif question ==  "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
+    elif question == "What layer from the TCP/IP model the protocol DHCP belongs to?":
         answer = 1
 
-    elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
+    elif question == "What layer of the TCP/IP model the protocol TCP belongs to?":
         answer = 4
-    return (answer)
+    return answer
 
 
 
@@ -38,34 +37,34 @@ def welcome_assignment_answers(question):
 
 if __name__ == "__main__":
     # use this space to debug and verify that the program works
-    debug_question = "Are encoding and encryption the same? - Yes/No"
+    debug_question = "Are encoding and encryption the same?"
     print(debug_question)
     print(welcome_assignment_answers(debug_question))
 
-    debug_question = "Is it possible to decrypt a message without a key? - Yes/No"
+    debug_question = "Is it possible to decrypt a message without a key?"
     print(debug_question)
     print(welcome_assignment_answers(debug_question))
 
-    debug_question = "Is it possible to decode a message without a key? - Yes/No"
+    debug_question = "Is it possible to decode a message without a key?"
     print(debug_question)
     print(welcome_assignment_answers(debug_question))
 
-    debug_question = "Is a hashed message supposed to be un-hashed? - Yes/No"
+    debug_question = "Is a hashed message supposed to be un-hashed?"
     print(debug_question)
     print(welcome_assignment_answers(debug_question))
 
-    debug_question = "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code"
+    debug_question = "What is the MD5 hashing value to the following message:"
     print(debug_question)
     print(welcome_assignment_answers(debug_question))
 
-    debug_question = "Is MD5 a secured hashing algorithm? - Yes/No"
+    debug_question = "Is MD5 a secured hashing algorithm?"
     print(debug_question)
     print(welcome_assignment_answers(debug_question))
 
-    debug_question = "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number"
+    debug_question = "What layer from the TCP/IP model the protocol DHCP belongs to?"
     print(debug_question)
     print(welcome_assignment_answers(debug_question))
 
-    debug_question = "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number"
+    debug_question = "What layer of the TCP/IP model the protocol TCP belongs to?"
     print(debug_question)
     print(welcome_assignment_answers(debug_question))
