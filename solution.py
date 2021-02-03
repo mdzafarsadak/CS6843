ff828f5fb6736918aa5da94109cf214918deaa40
MDZAFAR SADAK <37023641+mdzafarsadak@users.noreply.github.com>
2021-02-03 13:44:10 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,7 +2,7 @@ def welcome_assignment_answers(question):
     # The student doesn't have to follow the skeleton for this assignment.
     # Another way to implement is using a "case" statements similar to C.
     if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "Yes"
+        answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
@@ -16,5 +16,5 @@ def welcome_assignment_answers(question):
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
         answer = int("5")
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
-        answer = int("5")
+        answer = int("6")
     return (answer)
