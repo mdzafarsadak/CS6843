a581db220ba49e0ab74de2497ebd9bdd4a05d1c8
MDZAFAR SADAK <37023641+mdzafarsadak@users.noreply.github.com>
2021-02-03 20:16:03 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,4 +1,4 @@
-def welcome_assignment_answers(question):
+ef welcome_assignment_answers(question):
     if question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
 
@@ -10,6 +10,7 @@ def welcome_assignment_answers(question):
 
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = "No"
+
    
     elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
         answer = "42b76fe51778764973077a5a94056724"
@@ -17,9 +18,11 @@ def welcome_assignment_answers(question):
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
 
+
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
         answer = 5
         
+        
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
-        answer = 5
+        answer = 4
     return (answer)
